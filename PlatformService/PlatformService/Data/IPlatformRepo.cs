044dfce8a6fb61e8f3ab2ef556890bb322b89c57
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-21 14:39:14 +0300

    Implement platform repo

---
+++ PlatformService/PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,16 @@
+﻿using PlatformService.Models;
+using System.Collections.Generic;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+
+        Platform GetPlatformById(int id);
+
+        void CreatePlatform(Platform platform);
+    }
+}
---
+++ PlatformService/PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using PlatformService.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+            => this._context = context;
+
+        public bool SaveChanges()
+            => this._context.SaveChanges() >= 0;
+
+        public IEnumerable<Platform> GetAllPlatforms()
+            => this._context
+                .Platforms
+                .ToList();
+
+        public Platform GetPlatformById(int id)
+            => this._context
+                .Platforms
+                .FirstOrDefault(p => p.Id == id);
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            this._context
+                .Platforms
+                .Add(platform);
+        }
+    }
+}
