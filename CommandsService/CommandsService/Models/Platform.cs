5e24874373d0afe143c0c3abf21aa472f58b2b0d
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-31 21:41:36 +0200

    Create platform model in commands service

---
+++ CommandsService/CommandsService/Models/Platform.cs
@@ -0,0 +1,11 @@
+﻿namespace CommandsService.Models
+{
+    public class Platform
+    {
+        public int Id { get; init; }
+
+        public int ExternalId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
