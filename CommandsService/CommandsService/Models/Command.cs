0be7eb22b0fcc50c0c28dabd98d48e1380f8a7e2
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-31 21:52:49 +0200

    Create command database model

---
+++ CommandsService/CommandsService/Models/Command.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string CommandLine { get; set; }
+        
+        public int PlatformId { get; set; }
+
+        public Platform Platform { get; set; }
+    }
+}
--- CommandsService/CommandsService/Models/Platform.cs
+++ CommandsService/CommandsService/Models/Platform.cs
@@ -1,11 +1,18 @@
-﻿namespace CommandsService.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Models
 {
     public class Platform
     {
         public int Id { get; init; }
 
+        [Required]
         public int ExternalId { get; set; }
 
+        [Required]
         public string Name { get; set; }
+
+        public ICollection<Command> Commands { get; set; } = new HashSet<Command>();
     }
 }
