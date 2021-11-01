bd26b7722ba7d3d48fb3f3032cc74b941cb85752
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-11-01 10:25:35 +0200

    Implement command repo in command service

---
+++ CommandsService/CommandsService/Data/CommandRepo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using CommandsService.Models;
+using System.Collections.Generic;
+
+namespace CommandsService.Data
+{
+    public class CommandRepo : ICommandRepo
+    {
+        private readonly AppDbContext _context;
+
+        public CommandRepo(AppDbContext context)
+            => this._context = context;
+
+        public bool SaveChanges()
+            => this._context.SaveChanges() >= 0;
+
+        public IEnumerable<Platform> GetAllPlatforms()
+            => this._context.Platforms
+                .ToList();
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            this._context.Platforms.Add(platform);
+        }
+
+        public bool PlatformExits(int platformId)
+            => this._context.Platforms.Any(p => p.Id == platformId);
+
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+            => this._context.Commands
+                .Where(c => c.PlatformId == platformId)
+                .OrderBy(c => c.Platform.Name);
+
+        public Command GetCommand(int platformId, int commandId)
+            => this._context.Commands
+                .FirstOrDefault(c => c.PlatformId == platformId && c.Id == commandId);
+
+        public void CreateCommand(int platformId, Command command)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            command.PlatformId = platformId;
+            this._context.Commands.Add(command);
+        }
+    }
+}
---
+++ CommandsService/CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,23 @@
+﻿using CommandsService.Models;
+using System.Collections.Generic;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+
+        void CreatePlatform(Platform platform);
+
+        bool PlatformExits(int platformId);
+
+
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+
+        Command GetCommand(int platformId, int commandId);
+
+        void CreateCommand(int platformId, Command command);
+    }
+}
