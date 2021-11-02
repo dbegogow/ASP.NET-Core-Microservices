3fcbac4108c371f03ad58853fe5d8c067d863438
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-11-02 17:13:32 +0200

    Create commands controller and implement action method for getting commands from platform

---
+++ CommandsService/CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommandRepo repository, IMapper mapper)
+        {
+            this._repository = repository;
+            this._mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
+        {
+            Console.WriteLine($"--> Hit GetCommandsForPlatform: {platformId}");
+
+            if (!this._repository.PlatformExits(platformId))
+            {
+                return NotFound();
+            }
+
+            var commands = this._repository
+                .GetCommandsForPlatform(platformId);
+
+            return Ok(this._mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+    }
+}
