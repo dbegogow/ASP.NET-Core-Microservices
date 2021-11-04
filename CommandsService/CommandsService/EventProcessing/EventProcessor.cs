c677c7209994945c8d336298875438a36021959e
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-11-04 13:53:31 +0200

    Implement event processing

---
+++ CommandsService/CommandsService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using AutoMapper;
+using System.Text.Json;
+using CommandsService.Data;
+using CommandsService.DTOs;
+using CommandsService.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandsService.EventProcessing
+{
+    public class EventProcessor : IEventProcessor
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(IServiceScopeFactory scopeFactory, IMapper mapper)
+        {
+            this._scopeFactory = scopeFactory;
+            this._mapper = mapper;
+        }
+
+        public void ProcessEvent(string message)
+        {
+            var eventype = DetermineEvent(message);
+
+            switch (eventype)
+            {
+                case EventType.PlatformPublished:
+                    //TODO
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private EventType DetermineEvent(string notificationMessage)
+        {
+            Console.WriteLine("--> Determining Event");
+
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+
+            switch (eventType?.Event)
+            {
+                case "Platform_Published":
+                    Console.WriteLine("--> Platform Published Event Detected");
+                    return EventType.PlatformPublished;
+                default:
+                    Console.WriteLine("--> Could not determine the event type");
+                    return EventType.Undetermined;
+            }
+        }
+
+        private void AddPlatform(string platformPublishedMessage)
+        {
+            using (var scope = this._scopeFactory.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetRequiredService<ICommandRepo>();
+
+                var platformPublishedDto = JsonSerializer.Deserialize<PlatformPublishedDto>(platformPublishedMessage);
+
+                try
+                {
+                    var platform = this._mapper.Map<Platform>(platformPublishedDto);
+
+                    if (!repo.ExternalPlatformExists(platform.ExternalId))
+                    {
+                        repo.CreatePlatform(platform);
+                        repo.SaveChanges();
+                    }
+                    else
+                    {
+                        Console.WriteLine("--> Platform already exists...");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"--> Could not add Platform to DB {ex.Message}");
+                }
+            }
+        }
+    }
+
+    enum EventType
+    {
+        PlatformPublished,
+        Undetermined
+    }
+}
